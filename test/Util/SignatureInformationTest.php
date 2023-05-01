009797ee90547df20031ab4dcbb706862b172604
amahadaya <118939810+amahadaya@users.noreply.github.com>
2023-05-01 19:53:10 -0400

    Resolve VCSWP-19571 (Fix test bug for php sdk) (#31)
    * Fix test bug for php sdk
    
    * Extract timestamp into variable for php sdk
    
    * Extract MAX_INT to private variable

--- test/Util/SignatureInformationTest.php
+++ test/Util/SignatureInformationTest.php
@@ -8,10 +8,11 @@ use PHPUnit\Framework\TestCase;
 class SignatureInformationTest extends TestCase
 {
     protected $signatureInformation;
-
+    private int $timestamp = 1679944186;
+    private int $MAX_INTEGER = 2147483647;
     public function setUp(): void
     {
-        $requestHeader = "t=1679944186,v1=c3957749baf61df4b1506802579cc69a74c77a1ae21447b930e5a704f9ec4120,v1=1ba18712726898fbbe48cd862dd096a709f7ad761a5bab14bda9ac24d963a6a8";
+        $requestHeader = "t=" . strval(self::$timestamp) . ",v1=c3957749baf61df4b1506802579cc69a74c77a1ae21447b930e5a704f9ec4120,v1=1ba18712726898fbbe48cd862dd096a709f7ad761a5bab14bda9ac24d963a6a8";
         $this->signatureInformation = new SignatureInformation($requestHeader);
     }
 
@@ -23,7 +24,8 @@ class SignatureInformationTest extends TestCase
 
     public function testIsRequestTimeValidFalse()
     {
-        $tolerance = 5 * 60 * 10000;
+
+        $tolerance = self::$MAX_INTEGER - self::$timestamp;
         $this->assertEquals($this->signatureInformation->isRequestTimeValid($tolerance), false);
     }
 
