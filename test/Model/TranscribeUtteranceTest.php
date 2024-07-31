65b2549bb0fe368a84a7637c75002befbda19bf5
Ajevan M <amahadaya@vailsys.com>
2024-07-31 11:08:07 -0400

    Update openapi json file and transcribe utterance test description

--- test/Model/TranscribeUtteranceTest.php
+++ test/Model/TranscribeUtteranceTest.php
@@ -37,7 +37,7 @@ use PHPUnit\Framework\TestCase;
  * TranscribeUtteranceTest Class Doc Comment
  *
  * @category    Class
- * @description TranscribeUtterance
+ * @description The &#x60;TranscribeUtterance&#x60; command transcribes the caller’s voice and returns transcription of the audio and optionally returns the recording of the audio transcribed.  &#x60;TranscribeUtterance&#x60; is blocking and is a terminal command. As such, the actionUrl property is required, and control of the Call picks up using the &#x60;PerCL&#x60; returned in response of the &#x60;actionUrl&#x60;. Recording and Transcription information is returned in the actionUrl request. If the reason this command ended was due to the call hanging up, any PerCL returned will not execute.
  * @package     FreeClimb\Api
  * @author      OpenAPI Generator team
  * @link        https://openapi-generator.tech
